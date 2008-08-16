ac07957e4760c8f2fb148ca52772781520c99a2c
7destiny <7destiny@wowace>
2008-08-16 09:02:42 +0000

    - fix koKR

--- Localization/koKR.lua
+++ Localization/koKR.lua
@@ -94,7 +94,7 @@ DogTag_Unit_funcs[#DogTag_Unit_funcs+1] = function(DogTag_Unit, DogTag)
 	L["Focus"] = "주시"
 	L["Energy"] = "기력"
 	L["Mana"] = "마나"
-	L["Runic Power"] = "룬 마력",
+	L["Runic Power"] = "룬 마력"
 	
 	L["PvP"] = "전쟁" -- PVP
 	L["FFA"] = "전투 지역"
