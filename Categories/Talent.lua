dfb16662a6b0d3592b77ef6d6173d5d85eecba73
Parnic <parnic@parnic.com>
2010-10-18 05:02:58 +0000

    - fixed talent tags to pull the active talent group

--- Categories/Talent.lua
+++ Categories/Talent.lua
@@ -40,9 +40,10 @@ DogTag:AddAddonFinder("Unit", "LibStub", "LibTalentQuery-1.0", function(LibTalen
 	LibTalentQuery.RegisterCallback(DogTag_Unit, "TalentQuery_Ready", function(event, name, realm)
 		local fullName = realm and name .. "-" .. realm or name
 		
+		local talentGroup = GetActiveTalentGroup(true)
 		local data = newList()
 		for i = 1, 3 do
-			local _, name, _, _, points = GetTalentTabInfo(i, true)
+			local _, name, _, _, points = GetTalentTabInfo(i, true, false, talentGroup)
 			data[i] = newList(name, points)
 		end
 		update(fullName, data)
